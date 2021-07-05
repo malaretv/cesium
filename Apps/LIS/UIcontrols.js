0b64cab33f6b32c7b1277e726a4406449a622b0f
Calogero Mauceri <mauceri@actgate.com>
2021-07-05 15:28:01 +0200

    fixing Safari goto not being editable (testing)

--- Apps/LIS/UIcontrols.js
+++ Apps/LIS/UIcontrols.js
@@ -51,7 +51,7 @@ export function addGoToButton() {
   var gotoformHTML =
     "<form id='goto-form' hidden> \
             <p id='goto-error' hidden>Please fill out all fields.</p> \
-            <input list='locationsList' type='text' id='goto-input' placeholder='lon,lat[,ele (km)]' required /> \
+            <input list='locationsList' id='goto-input' placeholder='lon,lat[,ele (km)]' required /> \
             <datalist id='locationsList'></datalist> \
             <button type='submit'>Go</button> \
     </form>";
