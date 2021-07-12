efbc672b7ed15f5b37081632406e3feb0e1f2f48
Calogero Mauceri <mauceri@actgate.com>
2021-07-12 13:04:39 +0200

    remove undefined params from url string

--- Apps/LIS/utils.js
+++ Apps/LIS/utils.js
@@ -6,6 +6,13 @@
 export function updateUrlParams(params) {
   if (history.pushState) {
     var url = new URL(window.location);
+    // remove undefined url params
+    url.searchParams.forEach(function (value, key) {
+      if (params[key] === undefined) {
+        // remove param
+        url.searchParams.delete(key);
+      }
+    });
     for (var [key, value] of Object.entries(params)) {
       url.searchParams.set(key, value);
     }
