80d01a77e02c466213d75901cd5afedd40350c15
Calogero Mauceri <mauceri@actgate.com>
2021-07-27 15:09:15 +0200

    troubleshooting server errors on initialization when using raw QTS-3D (no built veresion)

--- Source/Scene/ShadowMap.js
+++ Source/Scene/ShadowMap.js
@@ -46,7 +46,7 @@ import Primitive from "./Primitive.js";
 import ShadowMapShader from "./ShadowMapShader.js";
 
 // TEMPORARY SOLUTION for fixing shadowmap computation when sun is low in the horizon!!!
-// import { invAdjustCartesianCoords } from "../../Apps/LIS/adjustCartesian.js";
+import { invAdjustCartesianCoords } from "../../Apps/LIS/adjustCartesian.js";
 import { isOptimizedPolarTerrain } from "../../Apps/LIS/terrainProvider.js";
 //////////////////////////
 
@@ -1453,26 +1453,25 @@ function checkVisibility(shadowMap, frameState) {
     // THE BEST THRESHOLD TO USE CHANGES DEPENDING ON THE LATITUDE
     // TO BE FIXED IN ANOTHER WAY!!!!!!!
 
-    // // computing current viewer latitude
-    // var cameraPosCarto = frameState.mapProjection.ellipsoid.cartesianToCartographic(
-    //   invAdjustCartesianCoords(sceneCamera.positionWC, isOptimizedPolarTerrain),
-    //   scratchCartesian1
-    // );
-    // var cameraLat = CesiumMath.toDegrees(cameraPosCarto.latitude);
+    // computing current viewer latitude
+    var cameraPosCarto = frameState.mapProjection.ellipsoid.cartesianToCartographic(
+      invAdjustCartesianCoords(sceneCamera.positionWC, isOptimizedPolarTerrain),
+      scratchCartesian1
+    );
+    var cameraLat = CesiumMath.toDegrees(cameraPosCarto.latitude);
 
-    // var polarLat = 75;
-    // var dotTh;
-    // if (Math.abs(cameraLat) < polarLat) {
-    //   dotTh = -0.00436;
-    // } else {
-    //   dotTh = -0.1;
-    // }
+    var polarLat = 75;
+    var dotTh;
+    if (Math.abs(cameraLat) < polarLat) {
+      dotTh = -0.00436;
+    } else {
+      dotTh = -0.1;
+    }
     // console.log("camera latitude " + cameraLat);
     // console.log("dotTh " + dotTh);
     // console.log("dot " + dot);
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    var dotTh = -0.1;
     if (dot < dotTh) {
       shadowMap._outOfView = true;
       shadowMap._needsUpdate = false;
